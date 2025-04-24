ba9accb846de04c497ae60b928d038c9d26e880a
maliiayik <maliiayik@gmail.com>
2025-04-24 23:34:57 +0300

    Meterial UI added

--- API/Program.cs
+++ API/Program.cs
@@ -28,10 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(opt =>
-{
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-});
+app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); });
 
 app.UseAuthorization();
 
