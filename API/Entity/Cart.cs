54511f363d51dae6485718444637adac83cd6d63
maliiayik <maliiayik@gmail.com>
2025-04-28 22:16:49 +0300

    Cart Object Methods added

--- API/Entity/Cart.cs
+++ API/Entity/Cart.cs
@@ -4,7 +4,37 @@ public class Cart
 {
     public int Id { get; set; }
     public string CustomerId { get; set; } = null!;
-    public List<CartItem> CartItems { get; set; } = new(); //cart oluşturulduğunda cartItem  referansı  bellekte oluşması için nesne örneği oluşturuyoruz.
+
+    public List<CartItem> CartItems { get; set; } =
+        new(); //cart oluşturulduğunda cartItem  referansı  bellekte oluşması için nesne örneği oluşturuyoruz.
+
+    public void AddItem(Product product, int quantity)
+    {
+        var item = CartItems.Where(c => c.ProductId == product.Id).FirstOrDefault();
+
+        if (item is null)
+        {
+            CartItems.Add(new CartItem { Product = product, Quantity = quantity });
+        }
+        else
+        {
+            item.Quantity += quantity;
+        }
+    }
+
+    public void RemoveItem(int productId, int quantity)
+    {
+        var item = CartItems.Where(c => c.ProductId == productId).FirstOrDefault();
+
+        if (item is null) return;
+
+        item.Quantity -= quantity;
+
+        if (item.Quantity == 0)
+        {
+            CartItems.Remove(item);
+        }
+    }
 }
 
 public class CartItem
