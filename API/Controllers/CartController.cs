277103fdba657cc5616842ba47eaa11214f85b5b
maliiayik <maliiayik@gmail.com>
2025-04-28 22:56:50 +0300

    CartController added

--- API/Controllers/CartController.cs
+++ API/Controllers/CartController.cs
@@ -12,14 +12,51 @@ public class CartController(DataContext context) : ControllerBase
     [HttpGet]
     public async Task<ActionResult<Cart>> GetCart()
     {
-        var cart = await context.Carts.Include(i => i.CartItems).ThenInclude(i => i.Product)
-            .Where(i => i.CustomerId == Request.Cookies["customerId"]).FirstOrDefaultAsync();
+        var cart = await GetOrCreate();
+        return Ok(cart);
+    }
 
-        if (cart is null)
+    [HttpPost]
+    public async Task<ActionResult> AddItemToCart(int productId, int quantity)
+    {
+        var cart = await GetOrCreate();
+
+
+        var product = await context.Products.FirstOrDefaultAsync(i => i.Id == productId);
+
+        if (product is null)
         {
-            return NotFound();
+            return NotFound("the product does not exist");
         }
 
-        return Ok(cart);
+        cart.AddItem(product, quantity);
+
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (result) return CreatedAtAction(nameof(GetCart), cart);
+
+        return BadRequest(new ProblemDetails { Title = "The Product can not be added to cart" });
+    }
+
+    private async Task<Cart> GetOrCreate()
+    {
+        var cart = await context.Carts.Include(i => i.CartItems).ThenInclude(i => i.Product)
+            .Where(i => i.CustomerId == Request.Cookies["customerId"]).FirstOrDefaultAsync();
+        if (cart is null)
+        {
+            var customerId = Guid.NewGuid().ToString();
+
+            var cookieOptions = new CookieOptions
+            {
+                Expires = DateTime.Now.AddMonths(1),
+                IsEssential = true
+            };
+            Response.Cookies.Append("customerId", customerId, cookieOptions);
+            cart = new Cart { CustomerId = customerId };
+            context.Carts.Add(cart);
+            await context.SaveChangesAsync();
+        }
+
+        return cart;
     }
 }
