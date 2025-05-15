71c4cead58d7ba50dd989b2b3cfaa0684fb5cd95
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2025-05-15 20:55:22 +0300

    counter slice added

--- Client/src/store/store.ts
+++ Client/src/store/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {counterSlice} from "../pages/counter/counterSlice.ts";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        counter: counterSlice.reducer
+    }
 });
 
 
