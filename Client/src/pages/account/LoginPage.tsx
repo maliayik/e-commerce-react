d8f07f4f0363e1f19302e064fbc079de002543d4
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2025-05-20 23:09:13 +0300

    Form Validation

--- Client/src/pages/account/LoginPage.tsx
+++ Client/src/pages/account/LoginPage.tsx
@@ -1,10 +1,11 @@
-import {Avatar, Box, Button, Container, Paper, TextField, Typography} from "@mui/material";
+import {Avatar, Box, Container, Paper, TextField, Typography} from "@mui/material";
 import {LockOutlined} from "@mui/icons-material";
 import {FieldValues, useForm} from "react-hook-form";
 import requests from "../../api/requests.ts";
+import {LoadingButton} from "@mui/lab";
 
 export default function LoginPage() {
-    const {register, handleSubmit} = useForm({
+    const {register, handleSubmit, formState: {errors, isSubmitting, isValid}} = useForm({
         defaultValues: {
             username: "",
             password: ""
@@ -24,20 +25,37 @@ export default function LoginPage() {
                 <Typography component={"h1"} variant={"h5"} sx={{textAlign: "center"}}>Login</Typography>
                 <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{mt: 2}}>
                     <TextField
-                        {...register("username")}
+                        {...register("username", {required: "username is required"})}
                         label={"Enter username"}
                         fullWidth required autoFocus
                         sx={{mb: 2}}
-                        size={"small"}> </TextField>
+                        size={"small"}
+                        error={!!errors.username}
+                        helperText={errors.username?.message}> </TextField>
+
                     <TextField
-                        {...register("password")}
+                        {...register("password", {
+                            required: "password is required", minLength: {
+                                value: 6,
+                                message: "Min length is 6 characters"
+                            }
+                        })}
                         label={"Enter password"}
                         type={"password"}
                         fullWidth required
                         sx={{mb: 2}}
-                        size={"small"}> </TextField>
+                        size={"small"}
+                        error={!!errors.password}
+                        helperText={errors.password?.message}> </TextField>
 
-                    <Button type={"submit"} variant={"contained"} fullWidth sx={{mt: 1}}>Login</Button>
+
+                    <LoadingButton
+                        loading={isSubmitting}
+                        disabled={!isValid}
+                        type={"submit"}
+                        variant={"contained"}
+                        fullWidth
+                        sx={{mt: 1}}>Login</LoadingButton>
 
                 </Box>
             </Paper>
